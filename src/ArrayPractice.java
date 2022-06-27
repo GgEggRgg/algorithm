a0f90b9eb7e70e117032e2acef0eea662bcf44c5
ggeggrgg <ggeggrgg@naver.com>
2022-06-27 18:06:34 +0900

    Create 0x01 practice2

---
+++ src/ArrayPractice.java
@@ -0,0 +1,40 @@
+public class ArrayPractice {
+
+    static int func2(int arr[], int len){
+        int temp[] = new int[101];
+        for (int i : arr) {
+            temp[i]++;
+        }
+        for(int i = 0; i < temp.length; i++) {
+            if(i > 50 && temp[i] != 0){
+                if(temp[50-(i-50)] != 0)
+                    return 1;
+            }
+            else if(i < 50 && temp[i] != 0) {
+                if(temp[50+(50-i)] != 0)
+                    return 1;
+            }
+            else {
+                if(temp[50] > 1)
+                    return 1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int arr[];
+        System.out.println("***** test1 *****");
+        arr = new int[] {1, 52, 48};
+        System.out.println(func2(arr, 3));
+
+        System.out.println("***** test2 *****");
+        arr = new int[] {50, 42};
+        System.out.println(func2(arr, 2));
+
+        System.out.println("***** test3 *****");
+        arr = new int[] {4, 13, 63, 87};
+        System.out.println(func2(arr, 4));
+
+    }
+}
